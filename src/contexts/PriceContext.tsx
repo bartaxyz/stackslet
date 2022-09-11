6bbf9b20ded5a4788641b52ee43eaedbfb4af72e
Ondřej Bárta <email@ondrejbarta.xyz>
2022-09-11 19:07:54 -0400

    Create PriceContext.tsx

---
+++ src/contexts/PriceContext.tsx
@@ -0,0 +1,42 @@
+import React, { createContext } from "react";
+import { useQuery } from "react-query";
+
+const PriceContext = createContext<{
+  price?: number;
+}>({});
+
+interface PriceProviderProps {
+  children: React.ReactNode;
+}
+
+interface CoingeckoPrice {
+  current_price: number;
+}
+
+export const PriceProvider = ({ children }: PriceProviderProps) => {
+  const { data: priceData } = useQuery<CoingeckoPrice[]>(
+    "price",
+    () =>
+      fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=blockstack",
+        {
+          method: "get",
+        }
+      ).then((res) => res.json()),
+    {
+      refetchInterval: 60000,
+    }
+  );
+
+  return (
+    <PriceContext.Provider
+      value={{
+        price: priceData?.[0]?.current_price,
+      }}
+    >
+      {children}
+    </PriceContext.Provider>
+  );
+};
+
+export const usePrice = () => React.useContext(PriceContext);
